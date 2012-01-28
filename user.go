b99a33d91d6a45208f84511c2202de33e5b62a7f
crazy2be <crazy1be@gmail.com>
2012-01-28 14:52:28 -0500

    Updated for latest weekly in anticipation of go 1.0

--- user.go
+++ user.go
@@ -2,19 +2,19 @@
 package user
 
 import (
-	"http"
-	"os"
+	"net/http"
+
 	// Local imports
 	"github.com/crazy2be/ini"
 	"github.com/crazy2be/session"
 )
 
 type User struct {
-	ID string // The user's ID, represented as an email in the current system.
+	ID       string            // The user's ID, represented as an email in the current system.
 	settings map[string]string // User settings, represented as a map[string]string right now. Might be represented as an interface{} later.
 }
 
-func Get(c http.ResponseWriter, r *http.Request) (u *User, err os.Error) {
+func Get(c http.ResponseWriter, r *http.Request) (u *User, err error) {
 	s := session.Get(c, r)
 	if err != nil {
 		return nil, err
@@ -25,7 +25,7 @@ func Get(c http.ResponseWriter, r *http.Request) (u *User, err os.Error) {
 	return
 }
 
-func GetExisting(r *http.Request) (u *User, err os.Error) {
+func GetExisting(r *http.Request) (u *User, err error) {
 	s, err := session.GetExisting(r)
 	if err != nil {
 		return nil, err
@@ -47,16 +47,16 @@ func (u *User) Set(key string, val string) {
 
 // Where are the user's settings stored?
 func (u *User) fileName() string {
-	return "data/shared/users/"+u.ID+"/settings"
+	return "data/shared/users/" + u.ID + "/settings"
 }
 
-func (u *User) Load() (err os.Error) {
+func (u *User) Load() (err error) {
 	filename := u.fileName()
 	u.settings, err = ini.Load(filename)
 	return err
 }
 
-func (u *User) Save() os.Error {
+func (u *User) Save() error {
 	filename := u.fileName()
 	err := ini.Save(filename, u.settings)
 	return err
